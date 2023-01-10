827b7c076cf5619680a4cf2053cbd20d350ea3d7
liav tausi <liavt242@gmail.com>
2023-01-10 22:56:00 +0200

    lessons

---
+++ edulabs/lessons_edulabs/lesson_16.py
@@ -0,0 +1,19 @@
+import base64
+import os
+import pprint
+from functools import lru_cache
+import argparse
+
+# encode terns the text to binary
+
+# decode terns the binary to base64 text
+#
+# url_id = base64.urlsafe_b64encode((r'https://edulabs.co.il/').encode()).decode().strip('=')
+# print(url_id)
+
+
+pprint.pprint(os.environ)
+
+# class
+
+
