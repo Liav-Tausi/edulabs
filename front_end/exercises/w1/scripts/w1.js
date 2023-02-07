94e3638bba90e140df94dcff3c11904cfa7e6cdf
Liav <liavt242@gmail.com>
2023-02-07 20:55:52 +0200

    front

---
+++ front_end/exercises/w1/scripts/w1.js
@@ -0,0 +1,47 @@
+"use strict"
+
+
+
+
+
+
+const myIMG = document.getElementById('myIMG');
+const myP = document.getElementById('myP');
+
+const myB = document.getElementById('myB');
+const showImgButton = document.getElementById('showImgButton')
+
+let dateDisplay = false;
+let imgDisplay = true;
+
+myB.addEventListener('click', showDate);
+showImgButton.addEventListener('click', showImg);
+
+function showDate() {
+
+  if (!dateDisplay) {
+    myP.innerHTML = new Date().toLocaleDateString('he-IS');
+    myB.innerHTML = 'Hide Date';
+    dateDisplay = true;
+  } else {
+    myP.innerHTML = '';
+    myB.innerHTML = 'Show Date';
+    dateDisplay = false;
+  }
+}
+
+function showImg() {
+
+  if (imgDisplay) {
+    myIMG.style.display = 'None';
+    showImgButton.innerHTML = 'Hide Img';
+    imgDisplay = false;
+  } else {
+    myIMG.style.display = 'block';
+    showImgButton.innerHTML = 'Show Img';
+    imgDisplay = true;
+  }
+}
+
+
+
